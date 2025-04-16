45f5676b60b44e34136ac8fe82401f17f2675cee
macprosha <evgebar-1@mail.ru>
2025-04-16 22:33:15 +0300

    next

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 export const UNKNOWN_ERROR = 's220s/unknown-error';
+export const OTHER_ERROR = 's220s/other-error';
 export const MONGO_CLIENT_CREATE_ERROR = 's220s/mongo/client/create/error';
 export const MONGO_CLIENT_CONNECT_ERROR = 's220s/mongo/client/connect/error';
+export const MONGO_DB_GET_ERROR = 's220s/mongo/db/get/error';
+export const MONGO_COLLECTION_CREATE_ERROR = 's220s/mongo/collection/create/error';
+export const MONGO_COLLECTION_GET_ERROR = 's220s/mongo/collection/get/error';
 export { getUiText } from './getUiText';
