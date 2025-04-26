e3edc93c0d2a55bbdf608a61a78969e1b91a0478
macprosha <evgebar-1@mail.ru>
2025-04-26 12:47:40 +0300

    next

--- index.ts
+++ index.ts
@@ -5,4 +5,5 @@ export const MONGO_CLIENT_CONNECT_ERROR = 's220s/mongo/client/connect/error';
 export const MONGO_DB_GET_ERROR = 's220s/mongo/db/get/error';
 export const MONGO_COLLECTION_CREATE_ERROR = 's220s/mongo/collection/create/error';
 export const MONGO_COLLECTION_GET_ERROR = 's220s/mongo/collection/get/error';
+export const MONGO_INSERT_ONE_NOT_ACKNOWLEDGED = 's220s/mongo/insertOne/not-acknowledged';
 export { getUiText } from './getUiText';
